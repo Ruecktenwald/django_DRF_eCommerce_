ce958ae5e8c2f322150e7bf336db48ea0bdf1f8b
Peter Ruecktenwald <Ruecktenwald@gmail.com>
2023-02-10 10:35:41 -0500

    Update serializers.py

--- drf_ecommerce/product/serializers.py
+++ drf_ecommerce/product/serializers.py
@@ -3,29 +3,33 @@ from .models import Product, Category, Brand, ProductLine
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source='name')
+
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = ['category_name', ]
 
 
 class BrandSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Brand
-        fields = '__all__'
+        exclude = ('id',)
 
 
 class ProductLineSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ProductLine
-        fields = '__all__'
+        exclude = ('id', 'product', 'is_active')
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    category = CategorySerializer()
-    brand = BrandSerializer()
+    brand_name = serializers.CharField(source='brand.name')
+    category_name = serializers.CharField(source='category.name')
     product_line = ProductLineSerializer(many=True)
 
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = ('id', 'name', 'slug', 'description',
+                  'brand_name', 'category_name', 'category', 'product_line',)
