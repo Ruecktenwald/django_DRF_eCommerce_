03af6b77f2d15ed79d543d62532852632a5eaf22
Peter Ruecktenwald <Ruecktenwald@gmail.com>
2023-02-14 13:43:05 -0500

    add resource tests

--- drf_ecommerce/product/models.py
+++ drf_ecommerce/product/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
+from .fields import OrderField
+from django.core.exceptions import ValidationError
+
+
+class ActiveManager(models.Manager):
+    # def get_queryset(self):
+    #     return super().get_queryset().filter(is_active=True)
+    def isactive(self):
+        return self.get_queryset().filter(is_active=True)
 
 
 class Category(MPTTModel):
     name = models.CharField(max_length=100)
     parent = TreeForeignKey('self', on_delete=models.PROTECT,
                             null=True, blank=True, related_name='children')
+    is_active = models.BooleanField(default=False)
+    objects = ActiveManager()
 
     class MPTTMeta:
         order_insertion_by = ['name']
@@ -16,6 +27,8 @@ class Category(MPTTModel):
 
 class Brand(models.Model):
     name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=False)
+    objects = ActiveManager()
 
     def __str__(self):
         return self.name
@@ -32,6 +45,7 @@ class Product(models.Model):
         Brand, on_delete=models.SET_NULL, null=True, blank=True)
 
     is_active = models.BooleanField(default=False)
+    objects = ActiveManager()
 
     def __str__(self):
         return self.name
@@ -45,6 +59,15 @@ class ProductLine(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name="product_line")
     is_active = models.BooleanField(default=False)
+    order = OrderField(unique_for_field="product", blank=True,)
+    objects = ActiveManager()
+
+    def clean_fields(self, exclude=None):
+        super().clean_fields(exclude=exclude)
+        qs = ProductLine.objects.filter(product=self.product)
+        for obj in qs:
+            if obj.id != self.id and obj.order == self.order:
+                raise ValidationError("Order must be unique for product")
 
     def __str__(self):
         return self.product.name
